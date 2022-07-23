6d135f56bc59ce1e4c1d96f509722c42792ce9cf
Simran Rai <simran_rai982@outlook.com>
2022-07-23 12:39:00 +0100

    map loads, style:changed background and added yoshi house marker, attempting to add popup window to marker

--- super-mario-world-map/script.js
+++ super-mario-world-map/script.js
@@ -2,8 +2,27 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 34.66794991523562, lng: 135.43024399172492 },
+    zoom: 18,
     mapId: "55b583199afff4a5",
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+  });
+  const markerYoshiHouse = new google.maps.Marker({
+    position: { lat: 34.66804360133394, lng: 135.43032705062683 },
+    map: map,
+    title: "Yoshi's House",
+    animation: google.maps.Animation.DROP,
+    icon: {
+      url: "./pics/yoshi_house.svg",
+      scaledSize: new google.maps.Size(38, 31),
+    },
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: "Yoshis House",
+  });
+  markerYoshiHouse.addListener("click", () => {
+    infowindow.open(map, marker);
   });
 }
